fea56009521f07d0d5152fa2d9f4b98c79a9fc53
mateasmario <mmmateas@gmail.com>
2023-03-19 12:52:34 +0200

    Opening another pie instance will now create a new tab in the existing instance

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic.ApplicationServices;
 
 namespace pie
 {
@@ -16,7 +17,60 @@ namespace pie
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            //Application.Run(new MainForm());
+
+            App myApp = new App();
+            myApp.Run(Environment.GetCommandLineArgs());
+        }
+    }
+
+    class App : WindowsFormsApplicationBase
+    {
+        public App()
+        {
+            // Make this a single-instance application
+            this.IsSingleInstance = true;
+            this.EnableVisualStyles = true;
+
+            // There are some other things available in 
+            // the VB application model, for
+            // instance the shutdown style:
+            this.ShutdownStyle =
+              Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses;
+
+            // Add StartupNextInstance handler
+            this.StartupNextInstance +=
+              new StartupNextInstanceEventHandler(this.SIApp_StartupNextInstance);
+        }
+
+        protected override void OnCreateMainForm()
+        {
+            // Create an instance of the main form 
+            // and set it in the application; 
+            // but don't try to run() it.
+            this.MainForm = new MainForm();
+
+            // We want to pass along the command-line arguments to 
+            // this first instance
+
+            // Allocate room in our string array
+            ((MainForm)this.MainForm).Args =
+                  new string[this.CommandLineArgs.Count];
+
+            // And copy the arguments over to our form
+            this.CommandLineArgs.CopyTo(((MainForm)this.MainForm).Args, 0);
+        }
+
+        protected void SIApp_StartupNextInstance(object sender, StartupNextInstanceEventArgs eventArgs)
+        {
+            // Copy the arguments to a string array
+            string[] args = new string[eventArgs.CommandLine.Count];
+            eventArgs.CommandLine.CopyTo(args, 0);
+
+            // Need to use invoke to b/c this is being called 
+            // from another thread.
+            this.MainForm.Invoke(new MainForm.ProcessParametersDelegate(
+                ((MainForm)this.MainForm).ProcessParameters), new object[] { args });
         }
     }
 }
