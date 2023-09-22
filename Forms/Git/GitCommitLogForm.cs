8190d52b942b49c9697e58b291e5ebd0f9ca07d3
mateasmario <mmmateas@gmail.com>
2023-09-22 16:06:38 +0300

    Added GNU license paragraph in source code files

--- Forms/Git/GitCommitLogForm.cs
+++ Forms/Git/GitCommitLogForm.cs
@@ -1,4 +1,23 @@
-﻿using pie.Classes;
+﻿/** Copyright (C) 2023  Mario-Mihai Mateas
+ * 
+ * This file is part of pie.
+ * 
+ * pie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * 
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+*/
+
+using pie.Classes;
 using pie.Services;
 using System;
 using System.Collections.Generic;
