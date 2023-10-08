7fb2a619e2cdde3734671d47e0a75f0174eeb326
mateasmario <mmmateas@gmail.com>
2023-10-08 08:33:42 +0300

    pie now simultaneously updates controls when changing between themes

---
+++ Classes/ControlHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pie.Classes
+{
+    public static class ControlHelper
+    {
+        #region Redraw Suspend/Resume
+        [DllImport("user32.dll", EntryPoint = "SendMessageA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        private const int WM_SETREDRAW = 0xB;
+
+        public static void SuspendDrawing(Control target)
+        {
+            SendMessage(target.Handle, WM_SETREDRAW, 0, 0);
+        }
+
+        public static void ResumeDrawing(Control target) { ResumeDrawing(target, true); }
+        public static void ResumeDrawing(Control target, bool redraw)
+        {
+            SendMessage(target.Handle, WM_SETREDRAW, 1, 0);
+
+            if (redraw)
+            {
+                target.Refresh();
+            }
+        }
+        #endregion
+    }
+}
