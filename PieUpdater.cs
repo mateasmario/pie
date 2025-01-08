9991576604b1a547a1feef10cabfc2fa261d5f3a
mateasmario <mmmateas@gmail.com>
2025-01-08 12:18:54 +0200

    250108.001: Finished pie automatic updater

--- PieUpdater.cs
+++ PieUpdater.cs
@@ -18,9 +18,11 @@
 */
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
+using System.Threading;
 
 namespace pie
 {
@@ -28,23 +30,56 @@ namespace pie
     {
         static void Main()
         {
-            using (var client = new WebClient())
+            try
             {
-                ServicePointManager.Expect100Continue = true;
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-                Console.WriteLine("Downloading latest release from GitHub...");
-                client.DownloadFile("https://github.com/mateasmario/pie/releases/latest/download/Release.zip", "Release.zip");
-                using (ZipArchive archive = ZipFile.OpenRead("Release.zip"))
+                using (var client = new WebClient())
                 {
-                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    ServicePointManager.Expect100Continue = true;
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                    Console.WriteLine("[PieUpdater] Downloading latest release from GitHub...");
+                    client.DownloadFile("https://github.com/mateasmario/pie/releases/latest/download/Release.zip", "Release.zip");
+                    Console.WriteLine("[PieUpdater] Download successful");
+
+                    ExtractArchive("Release.zip");
+
+                    File.Delete("Release.zip");
+                }
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("[PieUpdater] Update successful. Pie will now start.");
+
+            Thread.Sleep(2000);
+
+            Process.Start("pie.exe");
+        }
+
+        private static void ExtractArchive(string inputPath)
+        {
+            using (ZipArchive archive = ZipFile.OpenRead(inputPath))
+            {
+                Console.WriteLine("[PieUpdater] Deleting old files...");
+
+                foreach(ZipArchiveEntry archiveEntry in archive.Entries)
+                {
+                    Console.WriteLine("[PieUpdater] Deleting " + archiveEntry.FullName + "...");
+                    if (File.Exists(archiveEntry.FullName))
                     {
-                        if (entry.FullName != "PieUpdater.exe")
-                        {
-                            string destinationPath = Path.GetFullPath(Path.Combine(".", entry.FullName));
-                            entry.ExtractToFile(destinationPath);
-                        }
+                        File.Delete(archiveEntry.FullName);
+                    }
+                    if (Directory.Exists(archiveEntry.FullName))
+                    {
+                        Directory.Delete(archiveEntry.FullName, true);
                     }
                 }
+
+                Console.WriteLine("[PieUpdater] Old files deleted successfully");
+
+                Console.WriteLine("[PieUpdater] Extracting files from downloaded archive...");
+                archive.ExtractToDirectory(".");
+                Console.WriteLine("[PieUpdater] Files extracted successfully");
             }
         }
     }
