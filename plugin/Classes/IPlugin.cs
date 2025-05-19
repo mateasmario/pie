ffe5677286bfd84c6ebd2b6d484042aa4aeb0619
Mario-MihaiMateaș <mateasmario@ibm.com>
2025-05-19 20:53:45 +0300

    Added 'plugin' dependency

---
+++ plugin/Classes/IPlugin.cs
@@ -0,0 +1,9 @@
+﻿/* SPDX-FileCopyrightText: 2023-2025 Mario-Mihai Mateas <mateasmario@aol.com> */
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
+namespace pie.Classes;
+
+interface IPlugin
+{
+    List<PluginAction> onLoad(PluginContext context);
+}
---
+++ plugin/Classes/PluginContext.cs
@@ -0,0 +1,24 @@
+﻿/* SPDX-FileCopyrightText: 2023-2025 Mario-Mihai Mateas <mateasmario@aol.com> */
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
+using System.Collections.Generic;
+
+namespace pie.Classes;
+
+public class Metadata
+{
+    public string Version { get; set; }
+}
+
+public class Tab
+{
+    public bool IsFileOpened { get; set; }
+    public string FilePath { get; set; }
+    public string Text { get; set; }
+}
+
+public class PluginContext
+{
+    public Metadata Metadata { get; set; }
+    public List<Tab> Tabs { get; set; }
+}
